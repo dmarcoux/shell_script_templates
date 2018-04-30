767dc34d08ba6ed17a928b6dff4b36c5ce1856e7
Dany Marcoux <danymarcoux@gmail.com>
2018-04-30 12:56:48 +0200

    Add basic_with_env.sh

---
+++ basic_with_env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Lorem ipsum dolor sit amet (Write a short comment on what this script does)
+
+# Set working directory to this script's directory (It simplifies working with paths afterwards)
+cd "$(dirname "$0")" || exit
+
+# Setup environment
+source ./env
